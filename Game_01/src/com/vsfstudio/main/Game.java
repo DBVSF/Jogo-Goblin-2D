775ffeb4e22119816f714a7e3f83eeda41e4bf28
Darlan <Darlan@DESKTOP-FLHMQ8F>
2023-11-18 21:06:45 -0300

    movimento adicionado. Mapa e algumas sprites

--- Game_01/src/com/vsfstudio/main/Game.java
+++ Game_01/src/com/vsfstudio/main/Game.java
@@ -4,35 +4,61 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 
-public class Game extends Canvas implements Runnable {
+import com.vsfstudio.entities.Entity;
+import com.vsfstudio.entities.Player;
+import com.vsfstudio.graficos.Spritesheet;
+
+public class Game extends Canvas implements Runnable, KeyListener {
 
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	public static JFrame frame;
 	private Thread thread;
 	private final int WIDTH = 160;
 	private final int HEIGHT = 120;
 	private final int SCALE = 4;
 	private BufferedImage image;
+	private Player player;
+	
+	public List <Entity> entities;
+	public Spritesheet spritesheet;
 	
 	private boolean isRunning = true;
 	
 	public Game () {
 		
+	
+		addKeyListener(this);
 		setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
 		initFrame();
 		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		
+		entities = new ArrayList<Entity>();
+		
+		spritesheet = new Spritesheet("/spritesheet.png");
+		
+		player = new Player(0,0,16,16,spritesheet.getSprite(0, 11, 16, 16));
+		entities.add(player);
+		
 		
 	}
 	
 	
 	public void initFrame() {
 		
-		frame = new JFrame("GOBLIN DO AMOR");
+		frame = new JFrame("GOBLIN WAR");
 		frame.add(this);
 		frame.setResizable(false);
 		frame.pack();
@@ -50,7 +76,13 @@ public class Game extends Canvas implements Runnable {
 	}
 	
 	public synchronized void stop() {
-		
+		isRunning = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
@@ -62,7 +94,10 @@ public class Game extends Canvas implements Runnable {
 	}
 	
 	public void tick () {
-		
+		for(int i =0; i < entities.size(); i++) {
+			Entity e = entities.get(i);
+			e.tick();
+		}
 	}
 	
 	public void render () {
@@ -76,9 +111,19 @@ public class Game extends Canvas implements Runnable {
 		Graphics g = image.getGraphics();
 		g.setColor(new Color(0,0,0));
 		g.fillRect(0, 0, WIDTH, HEIGHT);
-		g= bs.getDrawGraphics();
+		
+		for(int i =0; i < entities.size(); i++) {
+			Entity e = entities.get(i);
+			e.render(g);
+		}
+		g.dispose();
+		g = bs.getDrawGraphics();
 		g.drawImage(image, 0,0, WIDTH*SCALE, HEIGHT*SCALE,null);
 		bs.show();
+		
+		
+		
+		
 	}
 	
 	public void run() {
@@ -96,11 +141,53 @@ public class Game extends Canvas implements Runnable {
 			if (delta >= 1 ) {
 				tick();
 				render();
+				
 			}
 			
 		}
+		stop();
+		
+	}
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
 		
 		
+	}
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+			if(e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
+				player.right = true;
+			}
+			else if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
+				player.left = true;
+			} 
+			if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
+				player.up = true;
+			}
+			else if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
+				player.down = true;
+			}
+		
+	}
+	public void keyReleased(KeyEvent e) {
+		
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
+			player.right = false;
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
+			player.left = false;
+		} 
+		if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
+			player.up = false;
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
+			player.down = false;
+		}
 	}
 
 	
